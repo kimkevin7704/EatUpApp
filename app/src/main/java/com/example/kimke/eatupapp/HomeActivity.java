5be4418ad3937494cb9e34b368b4c8500ac0a2fb
Kevin <kimkevin7704@gmail.com>
2018-04-06 16:06:26 -0500

    added profile page, button functionality in home page

--- app/src/main/java/com/example/kimke/eatupapp/HomeActivity.java
+++ app/src/main/java/com/example/kimke/eatupapp/HomeActivity.java
@@ -3,11 +3,14 @@ package com.example.kimke.eatupapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView userWelcome;
+    private Button recipes, home, profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,42 @@ public class HomeActivity extends AppCompatActivity {
         userWelcome = findViewById(R.id.userWelcome);
         userWelcome.setText("Welcome " + userName + "!");
 
+        recipes = findViewById(R.id.recipes);
+        home = findViewById(R.id.home);
+        profile = findViewById(R.id.profile);
+
+        recipes.setOnClickListener(this);
+        home.setOnClickListener(this);
+        profile.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.recipes:
+                openRecipeActivity();
+                break;
+            case R.id.home:
+                openHomeActivity();
+                break;
+            case R.id.profile:
+                openProfileActivity();
+                break;
+        }
+    }
+
+    public void openRecipeActivity() {
+        Intent intent = new Intent(this, RecipeActivity.class);
+        startActivity(intent);
+    }
+
+    public void openProfileActivity() {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
+
+    public void openHomeActivity() {
+        Intent intent = new Intent( this, HomeActivity.class);
+        startActivity(intent);
     }
 }
