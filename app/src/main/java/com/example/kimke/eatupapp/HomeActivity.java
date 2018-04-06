6c8e3ef7467016e7a4e838cd7f1391a97c8f6dab
DWings <porterderrellchase@gmail.com>
2018-04-06 18:00:15 -0500

    Removed Title Bar, added a Timer Activity(is a W.I.P).

--- app/src/main/java/com/example/kimke/eatupapp/HomeActivity.java
+++ app/src/main/java/com/example/kimke/eatupapp/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,6 +15,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
