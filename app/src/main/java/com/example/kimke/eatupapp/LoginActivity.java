e098633692545429b1b16ef4cabc781e4ab08bad
Kevin <kimkevin7704@gmail.com>
2018-04-06 14:52:10 -0500

    login/home started

---
+++ app/src/main/java/com/example/kimke/eatupapp/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.kimke.eatupapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText userNameInput;
+    private EditText userPasswordInput;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.loginToHome);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomeActivity();
+            }
+        });
+    }
+
+    public void openHomeActivity() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+    }
+}
